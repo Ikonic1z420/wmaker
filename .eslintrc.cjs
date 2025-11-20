4152e27b576645827dadf58c96e23fe35f5b173e
Ikonic1z420 <drokilla42@gmail.com>
2025-11-20 13:18:07 -0800

    chore: add package.json, ESLint/Prettier, Husky, lint scripts

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {}
+};
